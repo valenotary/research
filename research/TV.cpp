54da734936f3955be83d50ae1ae15bc924c45de2
Franz Anthony Varela <valenotary@gmail.com>
2019-09-04 22:44:30 -0700

    Updated TV.cpp (Final Naive Implementation)... Some errors need fixing

--- research/TV.cpp
+++ research/TV.cpp
@@ -14,13 +14,13 @@ for every Configuration in R2
 
 */
 
-#include <utility> //std::pair
-#include <iostream> //i/o
+#include <utility> // std::pair
+#include <iostream> // i/o
 #include <string> // writing strings
-#include <chrono> //timer
-#include <math.h> //pow
-#include <iterator> //std::set::iterator
-#include <algorithm> //std::max and std::min
+#include <chrono> // timer
+#include <math.h> // pow
+#include <iterator> // std::set::iterator
+#include <algorithm> // std::max and std::min
 #include <set> // for std::set
 #include <fstream> // writing to a file
 
@@ -33,14 +33,14 @@ struct Configuration {
 };
 
 //rudimentary helper function that checks if a configuration has already been placed inside the R2 set
-bool configFind(std::set<Configuration*> *R2, Configuration *c, int count) {
-  int place = 0;
+bool configFind(std::set<Configuration*> *R2, Configuration *c) {
+  //int place = 0;
   double c1 = c->config.first.first + 1000000;
   double c2 = c->config.first.second + 1000000;
   double d1 = c->config.second.first + 1000000;
   double d2 = c->config.second.second + 1000000;
   for (std::set<Configuration*>::iterator it = R2->begin(); it != R2->end(); it++) {
-    place++;
+    //place++;
     double a1 = (*it)->config.first.first + 1000000;
     double a2 = (*it)->config.first.second + 1000000;
     double b1 = (*it)->config.second.first + 1000000;
@@ -57,7 +57,7 @@ bool configFind(std::set<Configuration*> *R2, Configuration *c, int count) {
 //constructs the R2 set
 bool R2_Construct (double eps, std::set<Configuration*> *R2) {
   //The maximum job G can have is (1, 1)
-  std::ofstream outputFile("R2Construction.txt");
+  std::ofstream outputFile("TV.txt");
   int count = 0;
 
   //first add (0, 0) ; (0, 0)
@@ -94,10 +94,8 @@ bool R2_Construct (double eps, std::set<Configuration*> *R2) {
 
 
 
-	for (double g1 = 0; g1 <= l1; g1 += 1 / eps)
-	{
-		for (double g2 = 0; g2 <= l2; g2 += 1 / eps)
-		{
+	for (double g1 = 0; g1 <= l1; g1 += 1 / eps) {
+		for (double g2 = 0; g2 <= l2; g2 += 1 / eps) {
 			if (g1 == 0 && g2 == 0) continue;
       if (a1 + g1 > 2.00 || a2 + g2 > 2.00) continue;
 
@@ -105,86 +103,45 @@ bool R2_Construct (double eps, std::set<Configuration*> *R2) {
 			aMax = std::max(a1 + g1, a2 + g2);
 			bMax = std::max(b1 + g1, b2 + g2);
 
-			if (aMax < bMax)
-			{
-				++count;
-				Configuration *n = new Configuration(std::make_pair(a1 + g1, a2 + g2), std::make_pair(b1, b2));
-				if (!configFind(R2, n, count)) {outputFile << count << ": (" << a1 + g1 << ", " << a2 + g2 <<") ; (" << b1 << ", " << b2 << ")\n"; R2->insert(n);}
+      double trueMin = std::min(aMax, bMax);
+      Configuration *n;
+			if (trueMin == aMax) {
+				//++count;
+				n = new Configuration(std::make_pair(a1 + g1, a2 + g2), std::make_pair(b1, b2));
+				if (!configFind(R2, n)) {outputFile << "(" << a1 + g1 << ", " << a2 + g2 <<") ; (" << b1 << ", " << b2 << ")\n"; R2->insert(n);}
 			}
-			else if (bMax < aMax)
-			{
-				++count;
-				Configuration *n = new Configuration(std::make_pair(a1, a2), std::make_pair(b1 + g1, b2 + g2));
-				if (!configFind(R2, n, count)) {outputFile << count << ": (" << a1 << ", " << a2 <<") ; (" << b1 + g1 << ", " << b2 + g2 << ")\n"; R2->insert(n);}
-			}
-			else
-			{
-				Configuration *n1 = new Configuration(std::make_pair(a1 + g1, a2 + g2), std::make_pair(b1, b2));
-				Configuration *n2 = new Configuration(std::make_pair(a1, a2), std::make_pair(b1 + g1, b2 + g2));
-        ++count;
-				if (!configFind(R2, n1, count)) {outputFile << count << ": (" << a1 + g1 << ", " << a2 + g2 <<") ; (" << b1 << ", " << b2 << ")\n"; R2->insert(n1);}
-        ++count;
-				if (!configFind(R2, n2, count)) {outputFile << count << ": (" << a1 << ", " << a2 <<") ; (" << b1 + g1 << ", " << b2 + g2 << ")\n"; R2->insert(n2);}
+			if (trueMin == bMax) {
+				//++count;
+				n = new Configuration(std::make_pair(a1, a2), std::make_pair(b1 + g1, b2 + g2));
+				if (!configFind(R2, n)) {outputFile << "(" << a1 << ", " << a2 <<") ; (" << b1 + g1 << ", " << b2 + g2 << ")\n"; R2->insert(n);}
 			}
 		}
 	}
-  //  //add every job from (0, 0) -> (l1, l2) into R2
-  //  int i, j;
-  //  i = 0;
-  //  while (i <= eps*l1) {
-  //    j = 0;
-  //    while (j <= eps*l2) {
-  //      if (!(i == 0 && j == 0)) {
-  //        //insert the job accordingly
-  //        double g1 = i/eps;
-  //        double g2 = j/eps;
-
-  //        // std::cout << "i: " << i << std::endl;
-  //        // std::cout << "j: " << j << std::endl;
-  //        //
-  //        // std::cout << "g1: " << g1 << std::endl;
-  //        // std::cout << "g2: " << g2 << std::endl;
-
-  //        if ((g1 <= 1.00 && g2 <= 1.00) && !(g1 == 0 && g2 == 0)) {
-  //          if ((a1 + b1 + g1 <= 2.00 && a2 + b2 + g2 <= 2.00)) {
-  //            if (a1 == b1 && a2 == b2) {
-  //              count++;
-  //              Configuration *n1 = new Configuration (std::make_pair(a1 + g1, a2 + g2), std::make_pair(b1, b2));
-  //              if (!configFind(R2, n1, count)) R2->insert(n1); //else std::cout << "Config found! Current config not added" << std::endl;
-  //              count++;
-  //              Configuration *n2 = new Configuration (std::make_pair(a1, a2), std::make_pair(b1 + g1, b2 + g2));
-  //              if (!configFind(R2, n2, count)) R2->insert(n2); //else std::cout << "Config found! Current config not added" << std::endl;
-  //            } else {
-  //              count++;
-  //              double aMax, bMax;
-  //              aMax = std::max(a1 + g1, a2 + g2);
-  //              bMax = std::max(b1 + g1, b2 + g2);
-  //              double min = std::min(aMax, bMax);
-  //              Configuration *n;
-		//		if (min == aMax) {
-		//			n = new Configuration(std::make_pair(a1 + g1, a2 + g2), std::make_pair(b1, b2));
-		//		}
-		//		else if (min == bMax) {
-  //                n = new Configuration (std::make_pair(a1, a2), std::make_pair(b1 + g1, b2 + g2));
-  //              }
-		//		//
-  //              //if (!configFind(R2,n, count)) R2->insert(n); //else std::cout << "Config found! Current config not added" << std::endl;
-  //            }
-  //          }
-  //        }
-  //      }
-  //      j++;
-  //    }
-  //    //std::cout << "incrementing i" << std::endl;
-  //    i++;
-  //  }
-  }
+}
   outputFile.flush();
   outputFile.close();
   return true;
 }
 
 //Overloaded: if EVERY machine is > alpha - 1 on SOME dimension, then it is overloaded
+std::set<Configuration*> OVERLOAD(std::set<Configuration*> &R2, double alpha) {
+  std::set<Configuration*> Overloaded;
+  int count = 0;
+  for (std::set<Configuration*>::iterator it = R2.begin(); it != R2.end(); it++) {
+    double a1 = (*it)->config.first.first;
+    double a2 = (*it)->config.first.second;
+    double b1 = (*it)->config.second.first;
+    double b2 = (*it)->config.second.second;
+
+    //comment this out
+    if (count++ % 50 == 0)
+      std::cout << "aMax = " << std::max(a1, a2) << " ; bMax = " << std::max(b1, b2) << std::endl;
+
+    if ((std::max(a1, a2) + 1.0) > alpha && (std::max(b1, b2) + 1.0) > alpha) Overloaded.insert(new Configuration(std::make_pair(a1, a2), std::make_pair(b1, b2)));
+  }
+  return Overloaded;
+
+}
 // std::set<Configuration*> OVERLOADED (std::set<Configuration*> R2, double alpha) {
 //   std::set<Configuration*> R2_OVERLOAD;
 //   //iterate through R2 set
@@ -229,6 +186,22 @@ bool R2_Construct (double eps, std::set<Configuration*> *R2) {
 // }
 //
 // //Overflown: basically if the config creates a line segment that doesn't cross the unit box, then it is Overflown
+std::set<Configuration*> OVERFLOWN(std::set<Configuration*> &R2_set) { // should be passing in overloaded set
+    std::set<Configuration*> Overflown;
+    for (std::set<Configuration*>::iterator it = R2_set.begin(); it != R2_set.end(); it++) {
+      double x1, y1, x2, y2;
+      x1 = (*it)->config.first.first;
+      y1 = (*it)->config.first.second;
+      x2 = (*it)->config.second.first;
+      y2 = (*it)->config.second.second;
+
+      //(a1, a2) (b1, b2) = (0, 0) (1, 1)
+
+      //((y1−y2)(ax−x1)+(x2−x1)(ay−y1))((y1−y2)(bx−x1)+(x2−x1)(by−y1))<0.
+      if ((((y1-y2)*(0-x1)+(x2-x1)*(0-y1))*((y1-y2)*(1-x1)+(x2-x1)*(1-y1))) >= 0) Overflown.insert(new Configuration(std::make_pair(x1, y1), std::make_pair(x2, y2)));
+    }
+    return Overflown;
+}
 // std::set<Configuration*> OVERFLOWN (std::set<Configuration*> Overloaded) {
 //   std::set<Configuration*> OVERFLOWN;
 //   for (std::set<Configuration*>::iterator it = Overloaded.begin(); it != Overloaded.end(); it++) {
@@ -269,45 +242,16 @@ int main() {
   //Overloaded and Overflown checks here
 
   //Overloaded set
-  // int num, den;
-  // num = 8;
-  // den = 3;
-  // double alpha = (double) num / den;
-  // std::cout << "Construction R2 intersect L(" << num << "/" << den << ")..." << std::endl;
-  // std::cout << "Alpha: " << alpha << std::endl;
-  // std::set<Configuration*> R2_Overloaded = OVERLOADED(R2, alpha);
-  // std::cout << "Size of Overloaded set: " << R2_Overloaded.size() << std::endl;
-  // outputFile << "Overloaded set: \n";
-  // count = 0;
-  // for (std::set<Configuration*>::iterator it = R2_Overloaded.begin(); it != R2_Overloaded.end(); it++) {
-  //   count++;
-  //   double a1, a2, b1, b2;
-  //   a1 = (*it)->config.first.first;
-  //   a2 = (*it)->config.first.second;
-  //   b1 = (*it)->config.second.first;
-  //   b2 = (*it)->config.second.second;
-  //
-  //   //if (a1 + b1 > 2.00 || a2 + b2 > 2.00) R2.erase(configIterator(&R2, (*it)));
-  //   //std::cout << "(" << a1 << ", " << a2 <<") ; (" << b1 << ", " << b2 << ")" << std::endl;
-  //   outputFile << count << ": (" << a1 << ", " << a2 <<") ; (" << b1 << ", " << b2 << ")\n";
-  // }
-  // std::cout << "Creating Overflown set.. " << std::endl;
-  // std::set<Configuration*> R2Final = OVERFLOWN(R2_Overloaded);
-  // if (R2Final.empty()) std::cout << "algorithm is " << num << "/" << den << " competitive" << std::endl;
-  // else std::cout << "Error! Size of R2Final is: " << R2Final.size() << std::endl;
-  // outputFile << "\n Overflown set: \n";
-  // count = 0;
-  // for (std::set<Configuration*>::iterator it = R2Final.begin(); it != R2Final.end(); it++) {
-  //   count++;
-  //   double a1, a2, b1, b2;
-  //   a1 = (*it)->config.first.first;
-  //   a2 = (*it)->config.first.second;
-  //   b1 = (*it)->config.second.first;
-  //   b2 = (*it)->config.second.second;
-  //
-  //   //if (a1 + b1 > 2.00 || a2 + b2 > 2.00) R2.erase(configIterator(&R2, (*it)));
-  //   //std::cout << "(" << a1 << ", " << a2 <<") ; (" << b1 << ", " << b2 << ")" << std::endl;
-  //   outputFile << count << ": (" << a1 << ", " << a2 <<") ; (" << b1 << ", " << b2 << ")\n";
-  // }
-
+  int num, den;
+  num = 8;
+  den = 3;
+  double alpha = (double) num / den;
+  std::cout << "Construction R2 intersect L(" << num << "/" << den << ")..." << std::endl;
+  std::cout << "Alpha: " << alpha << std::endl;
+  std::set<Configuration*> R2_Overloaded = OVERLOAD(R2, alpha);
+  std::cout << "Finished creating Overloaded set; Size of Overloaded set: " << R2_Overloaded.size() << std::endl;
+  std::cout << "Checking Overloaded set for the Overflown values..." << std::endl;
+  std::set<Configuration*> R2_Overflown = OVERFLOWN(R2_Overloaded);
+  std::cout << "Overflown set size (from Overloaded): " << R2_Overflown.size() << std::endl;
+  if (R2_Overflown.empty()) std::cout << "Success! Algorithm is " << num << "/" << den << "-competitive!" << std::endl;
 }
